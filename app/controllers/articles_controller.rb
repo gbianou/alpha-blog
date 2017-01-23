6a6910d0aba0d841af9f7ebc2e54f45691a4dd60
Stan Bianou <gbianou@gmail.com>
2017-01-23 12:05:14 +0000

    Fix create, Add flash error and notification messages, Add Show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,20 @@ class ArticlesController < ApplicationController
     def create
         
         @article = Article.new(article_params)
-        @article.save
+        #@article.save
+        #redirect_to article_path(@article)
         
+        if @article.save 
+            flash[:notice] = "Your Article has been saved"
+            redirect_to article_path(@article)
+        else
+            render :new
+        end 
+    end 
+    
+    def show 
+        @article = Article.find(params[:id])
+            
     end 
     
     private 
