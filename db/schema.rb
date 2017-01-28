c3a90eaef93dd79509330b780ac0a8bde4e783f3
Stan Bianou <gbianou@gmail.com>
2017-01-28 12:17:51 +0000

    Create User

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170122125346) do
+ActiveRecord::Schema.define(version: 20170128120634) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
