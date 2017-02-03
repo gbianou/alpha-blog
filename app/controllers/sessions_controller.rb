f966befdbabca87b5499e1135708cd8dc3d52a6f
Stan Bianou <gbianou@gmail.com>
2017-02-03 01:48:40 +0000

    complete authentication system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
            session[:user_id] = user.id
-           flash[:sucess] = "You have been logged in"
+           flash[:success] = "You have been logged in"
            redirect_to user_path(user)
         else
             flash.now[:danger] = "Your username or email adress is incorect"
